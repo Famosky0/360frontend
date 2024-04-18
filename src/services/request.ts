118d6fe79be9b09eed9bf7547797b2226699cddd
Famosky0 <135935571+Famosky0@users.noreply.github.com>
2024-04-18 19:17:22 +0100

    Update request.ts

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,33 +1,7 @@
 import axios from "axios";
 import { notify, notifyError } from "./toastify";
-import { access } from "fs";
-import { error } from "console";
-import {
-  bookingSchema,
-  loginProps,
-  registerProps,
-  OTPDetails,
-  calculateSchema,
-} from "@/app/dashboard/components/Interface";
 
 const api = "https://api.360shotitz.com/api/v1";
-
-// export function getCookie(cookieName: string) {
-//   const name = cookieName + "=";
-//   const decodedCookie = decodeURIComponent(document.cookie);
-//   const cookieArray = decodedCookie.split(";");
-
-//   for (let i = 0; i < cookieArray.length; i++) {
-//     let cookie = cookieArray[i];
-//     while (cookie.charAt(0) === " ") {
-//       cookie = cookie.substring(1);
-//     }
-//     if (cookie.indexOf(name) === 0) {
-//       return cookie.substring(name.length, cookie.length);
-//     }
-//   }
-//   return null; // Return null if the cookie is not found
-// }
 
 export const setConfig = (accessToken: string) => {
   const config = {
@@ -88,12 +62,7 @@ const refreshToken = async () => {
     })
     .catch((err) => {
       console.log("refresh error");
-      if (err.response.data.message) {
-        console.log(err.response.data.message);
-        // notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
+      notifyError("Network Error");
       console.log(err);
     });
 };
@@ -101,7 +70,6 @@ const refreshToken = async () => {
 axios.interceptors.response.use(
   (response) => {
     console.log("responding...");
-    // If the response is successful, return it directly
     return response;
   },
   async (error) => {
@@ -110,16 +78,12 @@ axios.interceptors.response.use(
       error.config.url !== "/auth/register/" &&
       error.response
     ) {
-      // If the error is a 401, attempt to refresh the token
       if (error.response.status === 401 && !error.config._isRetry) {
-        // Mark the request for retry to prevent an infinite loop
         error.config._isRetry = true;
         try {
-          // Refresh the token
+          await refreshToken();
           const Type = localStorage.getItem("Type");
           console.log("automatically refreshing token...");
-          await refreshToken();
-          console.log(Type);
           const updatedConfig = {
             ...error.config,
             headers: {
@@ -129,22 +93,18 @@ axios.interceptors.response.use(
               )}`,
             },
           };
-          // Retry the original request
           return axios(updatedConfig);
-          // return axios(error.config);
         } catch (refreshError) {
           console.log("refresh error");
-          // Handle token refresh failure
           return Promise.reject(refreshError);
         }
       }
     }
-    // For other errors, reject the promise
     return Promise.reject(error);
   }
 );
 
-export const userLogin = async (data: loginProps, router: any) => {
+export const userLogin = async (data, router) => {
   await axios
     .post(`${api}/auth/login/`, data, {
       withCredentials: true,
@@ -154,23 +114,18 @@ export const userLogin = async (data: loginProps, router: any) => {
       if (response.data.status === "success") {
         localStorage.setItem("refreshToken", response.data.data.refresh);
         localStorage.setItem("accessToken", response.data.data.access);
-        console.log(response.data.data);
         console.log("authenticated user login");
         notify(response.data.message);
         router.push("/dashboard");
       }
     })
     .catch((err) => {
-      if (err.response.data.message) {
-        notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
+      notifyError("Network Error");
       console.log(err);
     });
 };
 
-export const userRegistration = async (data: registerProps) => {
+export const userRegistration = async (data) => {
   await axios
     .post(`${api}/auth/register/`, data, {
       headers: {
@@ -181,101 +136,12 @@ export const userRegistration = async (data: registerProps) => {
     .then((response) => {
       if (response.data.message) {
         localStorage.setItem("userEmail", data.email);
-      }
-      notify(response.data.message);
-      window.location.pathname = "/auth/login";
-    })
-    .catch((err) => {
-      if (err.response.data.message) {
-        notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
-      console.log(err);
-    });
-};
-
-export const resetPasswordOTP = async (email: string) => {
-  await axios
-    .post(
-      `${api}/auth/send-password-reset-otp/`,
-      { email },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        withCredentials: true,
-      }
-    )
-    .then((response) => {
-      console.log(response);
-      if (response.data.message) {
         notify(response.data.message);
+        window.location.pathname = "/login";
       }
     })
     .catch((err) => {
-      if (err.response.data.message) {
-        notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
-      console.log(err);
-    });
-};
-
-export const verifyOTP = async (
-  data: OTPDetails,
-  router: any,
-  type: string
-) => {
-  await axios
-    .post(`${api}/auth/verify-email/`, data, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      withCredentials: true,
-    })
-    .then((response) => {
-      console.log(response);
-      if (response.data.message) {
-        notify(response.data.message);
-        if ((type = "registeration")) {
-          router.push("/auth/login");
-        } else {
-          router.push("/dashboard/profile");
-        }
-      }
-    })
-    .catch((err) => {
-      if (err.response.data.message) {
-        notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
-      console.log(err);
-    });
-};
-
-export const resendVerificationOTP = async (data: any) => {
-  await axios
-    .post(`${api}/auth/resend-verification-email/`, data, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      withCredentials: true,
-    })
-    .then((response) => {
-      console.log(response);
-      if (response.data.message) {
-        notify(response.data.message);
-      }
-    })
-    .catch((err) => {
-      if (err.response.data.message) {
-        notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
+      notifyError("Network Error");
       console.log(err);
     });
 };
@@ -286,221 +152,11 @@ export const logOutUser = async (accessToken: string) => {
     .then((response) => {
       console.log(response);
       if (response.data.status === "success") {
-        console.log(response.data.message);
+        console.log("Logged out successfully");
       }
     })
     .catch((err) => {
-      if (err.response.data.message) {
-        notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
-    });
-};
-
-export const recentBookingsAndImages = async (accessToken: string) => {
-  let result: any = [];
-  await axios
-    .get(`${api}/store/bookings-and-images/`, setConfig(accessToken))
-    .then((response) => {
-      console.log(response);
-      if (response.data.status === "success") {
-        result = response.data.data;
-        console.log(response.data.message);
-      }
-    })
-    .catch((err) => {
-      if (err.response.data.message) {
-        notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
-    });
-  return result;
-};
-
-export const retrieveAllUserBookings = async (accessToken: string) => {
-  let result: any = [];
-  await axios
-    .get(`${api}/store/bookings/`, setConfig(accessToken))
-    .then((response) => {
-      console.log(response);
-      if (response.data.status === "success") {
-        result = response.data.data;
-        console.log(response.data.message);
-      }
-    })
-    .catch((err) => {
-      if (err.response.data.message) {
-        notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
-    });
-  return result;
-};
-
-export const calculateAmount = async (
-  accessToken: string,
-  data: calculateSchema
-) => {
-  let result: any = [];
-  await axios
-    .post(`${api}/store/calculate-amount/`, data, setConfig(accessToken))
-    .then((response) => {
-      console.log(response);
-      if (response.data.status === "success") {
-        result = response.data.data;
-        console.log("amount calculated");
-      }
-    })
-    .catch((err) => {
-      if (err.response.data.message) {
-        notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
+      notifyError("Network Error");
       console.log(err);
     });
-  return result;
-};
-
-export const createBookings = async (data: bookingSchema) => {
-  let result: any = [];
-  await axios
-    .post(
-      `${api}/store/bookings/`,
-      data.location
-        ? data
-        : {
-            phone: data.phone,
-            plan: data.plan,
-            shoot_type: data.shoot_type,
-            number_of_shoot: data.number_of_shoot,
-            shooting_date: data.shooting_date,
-            shooting_time: data.shooting_time,
-          },
-      setConfig("accessToken")
-    )
-    .then((response) => {
-      console.log(response);
-      if (response.data.status === "success") {
-        notify(response.data.message);
-        result = response.data.data;
-        console.log("bookings created");
-      }
-    })
-    .catch((err) => {
-      if (err.response.data.message) {
-        notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
-      console.log(err);
-    });
-  return result;
-};
-
-export const getPhotos = async (accessToken: string) => {
-  let result: any = [];
-  await axios
-    .get(`${api}/profiles/photos/`, setConfig(accessToken))
-    .then((response) => {
-      console.log(response);
-      if (response.data.status === "success") {
-        result = response.data.data;
-        console.log(response.data.message);
-      }
-    })
-    .catch((err) => {
-      if (err.response.data.message) {
-        notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
-    });
-  return result;
-};
-
-export const retrieveProfile = async (accessToken: string) => {
-  let result: any = [];
-  await axios
-    .get(`${api}/profiles/`, setConfig(accessToken))
-    .then((response) => {
-      console.log(response);
-      if (response.data.status === "success") {
-        result = response.data.data;
-        console.log(response.data.message);
-      }
-    })
-    .catch((err) => {
-      if (err.response.data.message) {
-        notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
-    });
-  return result;
-};
-
-export const updateProfile = async (data: any) => {
-  await axios
-    .patch(`${api}/profiles/`, data, setProfileConfig("accessToken"))
-    .then((response) => {
-      console.log(response);
-      if (response.data.status === "success") {
-        notify(response.data.message);
-        console.log(response.data.message);
-      }
-    })
-    .catch((err) => {
-      if (err.response.data.message) {
-        notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
-      console.log(err);
-    });
-};
-
-export const retrieveAvailablePlans = async (accessToken: string) => {
-  let result: any = [];
-  await axios
-    .get(`${api}/store/plans/`, setConfig(accessToken))
-    .then((response) => {
-      console.log(response);
-      if (response.data.status === "success") {
-        result = response.data.data;
-        console.log(response.data.message);
-      }
-    })
-    .catch((err) => {
-      if (err.response.data.message) {
-        notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
-    });
-  return result;
-};
-
-export const retrieveBankDetails = async (accessToken: string) => {
-  let result: any = [];
-  await axios
-    .get(`${api}/store/account-detail/`, setConfig(accessToken))
-    .then((response) => {
-      console.log(response);
-      if (response.data.status === "success") {
-        result = response.data.data;
-        console.log(response.data.message);
-      }
-    })
-    .catch((err) => {
-      if (err.response.data.message) {
-        notifyError(err.response.data.message);
-      } else {
-        notifyError("Network Error");
-      }
-    });
-  return result;
 };
