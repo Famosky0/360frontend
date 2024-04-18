8348a3b90ad7e2f40a6584e021f0fa16dabb840f
Famosky0 <135935571+Famosky0@users.noreply.github.com>
2024-04-18 05:50:13 +0100

    Update StepOne.tsx update

--- src/app/dashboard/components/Bookings/StepOne.tsx
+++ src/app/dashboard/components/Bookings/StepOne.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { bookingSchema, profileSchema } from "../Interface";
 import { retrieveProfile } from "@/services/request";
 
@@ -15,10 +15,10 @@ const BookingProcessOne = ({
   profile: profileSchema;
   setProfile: React.Dispatch<React.SetStateAction<profileSchema>>;
 }) => {
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState(""); // State to store error messages
 
   const handleChange = (e: any) => {
-    const { name, value } = e.target;
+    let { name, value } = e.target;
 
     // Reset error message
     if (name === "phone") {
@@ -48,20 +48,68 @@ const BookingProcessOne = ({
     }
   };
 
+  useEffect(() => {
+    getUserProfile();
+  }, []);
+
   return (
-    <div>
-      <form>
-        {/* Example input for phone number */}
-        <input
-          type="tel"
-          id="Phone_number"
-          name="phone"
-          value={bookingInfo["phone"]}
-          onChange={handleChange}
-          placeholder="+2348149055068"
-          className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
-        />
-        {errorMessage && <div className="text-red-500 text-sm mt-2">{errorMessage}</div>}
+    <div className="w-full flex flex-col gap-4">
+      <div className="w-full flex flex-col gap-2">
+        <h1 className="text-3xl text-primary">Create Bookings</h1>
+      </div>
+
+      <form className="flex flex-col gap-5 mt-8">
+        <div>
+          <label htmlFor="full_name">Full Name</label>
+          <input
+            type="text"
+            id="full_name"
+            name="full_name"
+            value={profile.first_name + " " + profile.last_name}
+            disabled
+            placeholder="Enter your Full Name"
+            className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
+          />
+        </div>
+        <div>
+          <label htmlFor="Phone_number">Your Whatsapp Number</label>
+          <input
+            type="tel"
+            id="Phone_number"
+            name="phone"
+            value={bookingInfo["phone"]}
+            onChange={handleChange}
+            placeholder="+2348149055068"
+            pattern="^\+234\d{10}$"
+            className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
+          />
+          {errorMessage && <div className="text-red-500 text-sm mt-2">{errorMessage}</div>}
+        </div>
+
+        <div>
+          <label htmlFor="password">Date</label>
+          <input
+            type="date"
+            id="date"
+            name="shooting_date"
+            value={bookingInfo["shooting_date"]}
+            onChange={handleChange}
+            placeholder={new Date().getTime.toString()}
+            className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
+          />
+        </div>
+        <div>
+          <label htmlFor="time">Time (When are you coming for your shoot)</label>
+          <input
+            type="time"
+            id="time"
+            name="shooting_time"
+            value={bookingInfo["shooting_time"]}
+            onChange={handleChange}
+            placeholder={new Date().getTime.toString()}
+            className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
+          />
+        </div>
       </form>
     </div>
   );
