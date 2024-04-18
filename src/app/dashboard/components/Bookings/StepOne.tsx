029bbb03618c68f0f915fbb926fdfa87f1852b11
Famosky0 <135935571+Famosky0@users.noreply.github.com>
2024-04-18 11:27:39 +0100

    Update StepOne.tsx mm

--- src/app/dashboard/components/Bookings/StepOne.tsx
+++ src/app/dashboard/components/Bookings/StepOne.tsx
@@ -1,50 +1,24 @@
 "use client";
+import React, { useState } from "react";
 
-import React, { useEffect, useState } from "react";
-import { bookingSchema, profileSchema } from "../Interface";
-import { retrieveProfile } from "@/services/request";
+const BookingProcessOne = ({ profile }) => {
+  const [bookingInfo, setBookingInfo] = useState({
+    phone: "",
+    shooting_date: "",
+    shooting_time: "",
+  });
 
-const BookingProcessOne = ({
-  setBookingInfo,
-  bookingInfo,
-  profile,
-  setProfile,
-}: {
-  setBookingInfo: React.Dispatch<React.SetStateAction<bookingSchema>>;
-  bookingInfo: bookingSchema;
-  profile: profileSchema;
-  setProfile: React.Dispatch<React.SetStateAction<profileSchema>>;
-}) => {
-  const handleChange = (e: any) => {
-    let name = e.target.name;
-    let value = e.target.value;
-    setBookingInfo({ ...bookingInfo, [name]: value });   
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setBookingInfo((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
-
-  const getUserProfile = async () => {
-    let data = [];
-    const accessToken = localStorage.getItem("accessToken");
-    console.log("token: " + accessToken);
-    if (accessToken) {
-      data = await retrieveProfile(accessToken);
-      console.log(data);
-      if (data) {
-        setProfile(data);
-      }
-    } else {
-      data = await retrieveProfile("string");
-    }
-  };
-
-  useEffect(() => {
-    getUserProfile();
-  }, []);
 
   return (
-    <div className="w-full flex flex-col gap-4">
-      <div className="w-full flex flex-col gap-2">
-        <h1 className="text-3xl text-primary">Create Bookings</h1>
-      </div>
+    <div className="w-full flex flex-col gap-2">
+      <h1 className="text-3xl text-primary">Create Bookings</h1>
 
       <form className="flex flex-col gap-5 mt-8">
         <div>
@@ -62,16 +36,16 @@ const BookingProcessOne = ({
         <div>
           <label htmlFor="Phone_number">WhatsApp Number</label>
           <input
-            type="number"
+            type="tel"
             id="Phone_number"
             name="phone"
             value={bookingInfo["phone"]}
             onChange={handleChange}
-            placeholder="+2348149055068"
+            placeholder="08036399878"
+            pattern="^\d{11}$"
             className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
           />
         </div>
-
         <div>
           <label htmlFor="password">Date</label>
           <input
@@ -80,19 +54,19 @@ const BookingProcessOne = ({
             name="shooting_date"
             value={bookingInfo["shooting_date"]}
             onChange={handleChange}
-            placeholder={new Date().getTime.toString()}
+            placeholder={new Date().toISOString().split('T')[0]}
             className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
           />
         </div>
         <div>
-          <label htmlFor="time">Time (when are you shooting?) </label>
+          <label htmlFor="time">Time (when are you shooting?)</label>
           <input
             type="time"
             id="time"
             name="shooting_time"
             value={bookingInfo["shooting_time"]}
             onChange={handleChange}
-            placeholder={new Date().getTime.toString()}
+            placeholder="HH:MM"
             className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
           />
         </div>
