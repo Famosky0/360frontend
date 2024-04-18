edfe93fae9083148cc7c75b0e8a4e865de94771b
Famosky0 <135935571+Famosky0@users.noreply.github.com>
2024-04-18 18:42:46 +0100

    Update StepOne.tsx

--- src/app/dashboard/components/Bookings/StepOne.tsx
+++ src/app/dashboard/components/Bookings/StepOne.tsx
@@ -1,48 +1,98 @@
-import React from 'react';
+"use client";
 
-const BookingProcessOne = ({ setBookingInfo, bookingInfo, profile }) => {
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setBookingInfo(prev => ({ ...prev, [name]: value }));
+import React, { useEffect, useState } from "react";
+import { bookingSchema, profileSchema } from "../Interface";
+import { retrieveProfile } from "@/services/request";
+
+const BookingProcessOne = ({
+  setBookingInfo,
+  bookingInfo,
+  profile,
+  setProfile,
+}: {
+  setBookingInfo: React.Dispatch<React.SetStateAction<bookingSchema>>;
+  bookingInfo: bookingSchema;
+  profile: profileSchema;
+  setProfile: React.Dispatch<React.SetStateAction<profileSchema>>;
+}) => {
+  const handleChange = (e: any) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setBookingInfo({ ...bookingInfo, [name]: value });   
   };
+
+  const getUserProfile = async () => {
+    let data = [];
+    const accessToken = localStorage.getItem("accessToken");
+    console.log("token: " + accessToken);
+    if (accessToken) {
+      data = await retrieveProfile(accessToken);
+      console.log(data);
+      if (data) {
+        setProfile(data);
+      }
+    } else {
+      data = await retrieveProfile("string");
+    }
+  };
+
+  useEffect(() => {
+    getUserProfile();
+  }, []);
 
   return (
     <div className="w-full flex flex-col gap-4">
       <div className="w-full flex flex-col gap-2">
         <h1 className="text-3xl text-primary">Create Bookings</h1>
       </div>
+
       <form className="flex flex-col gap-5 mt-8">
         <div>
-          <label htmlFor="Phone_number">Your WhatsApp Number</label>
+          <label htmlFor="full_name">Full Name</label>
           <input
             type="text"
-            id="Phone_number"
-            name="phone"
-            value={bookingInfo.phone}
-            onChange={handleChange}
-            placeholder="08036399878"
+            id="full_name"
+            name="full_name"
+            value={profile.first_name + " " + profile.last_name}
+            disabled
+            placeholder="Enter your Full Name"
             className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
           />
         </div>
         <div>
-          <label htmlFor="date">Date</label>
+          <label htmlFor="Phone_number">WhatsApp Number</label>
+          <input
+            type="number"
+            id="Phone_number"
+            name="phone"
+            value={bookingInfo["phone"]}
+            onChange={handleChange}
+            placeholder="+2348149055068"
+            className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Date</label>
           <input
             type="date"
             id="date"
             name="shooting_date"
-            value={bookingInfo.shooting_date}
+            value={bookingInfo["shooting_date"]}
             onChange={handleChange}
+            placeholder={new Date().getTime.toString()}
             className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
           />
         </div>
         <div>
-          <label htmlFor="time">Time (when are you shooting?)</label>
+          <label htmlFor="time">Time (when are you shooting?) </label>
           <input
             type="time"
             id="time"
             name="shooting_time"
-            value={bookingInfo.shooting_time}
+            value={bookingInfo["shooting_time"]}
             onChange={handleChange}
+            placeholder={new Date().getTime.toString()}
             className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
           />
         </div>
